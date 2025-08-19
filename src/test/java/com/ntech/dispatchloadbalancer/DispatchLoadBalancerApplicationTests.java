4ff6619c992b9e90ee415421c7282f7c37886e53
neelesh <neeleshnani99@gmail.com>
2025-08-19 15:08:53 +0530

    initiallized spring boot application

---
+++ src/test/java/com/ntech/dispatchloadbalancer/DispatchLoadBalancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntech.dispatchloadbalancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispatchLoadBalancerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
